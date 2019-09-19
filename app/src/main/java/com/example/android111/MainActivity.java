577818a01385a6056103aeaac4035766e19119f8
AzureSoda <dmb07250@gmail.com>
2019-09-19 22:20:51 +0900

    0919

--- app/src/main/java/com/example/android111/MainActivity.java
+++ app/src/main/java/com/example/android111/MainActivity.java
@@ -3,19 +3,58 @@ package com.example.android111;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
+
+        Log.v(TAG,"onCreate()");
     }
 
     @Override
-    public void onBackPressed() {
-        Toast.makeText(this,"봙봙111111!", Toast.LENGTH_LONG).show();
+    protected void onStart() {
+        super.onStart();
+        Log.v(TAG,"onStart()");
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.v(TAG,"Resume()");
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        Log.v(TAG,"OnPause()");
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        Log.v(TAG,"OnStop()");
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        Log.v(TAG,"OnDestroy()");
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        Toast.makeText(this,"봙mmmmmmmmmm봙!", Toast.LENGTH_LONG).show();
+    }
+
 }
+
+
